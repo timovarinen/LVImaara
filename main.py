77479acdcd9f150a32dd8b38217b4a5c4fbe3533
timovarinen <varinen.timo@gmail.com>
2024-02-22 11:54:06 +0200

    ADD function to count pipe meters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ifcopenshell
+import ifcopenshell.util.element
 
 
 def read_file():
@@ -15,3 +16,34 @@ def read_file():
     except IOError:
         print("Could not open ", filePath)
         return 0
+
+def pipe_meters(model):
+    """
+    Read and count pipe meters form model
+
+    Parameters:
+        model: model to be read
+
+    Returns:
+        dict: quantity of pipes as {type : {DN : qty}}
+    """
+
+    pipes = model.by_type("IfcPipeSegment")
+    qty = {}
+
+    for pipe in pipes:
+        
+        name = pipe.Name
+        DN = ifcopenshell.util.element.get_pset(pipe, "FI_Geometria", prop="Koko (DN)")
+        length = ifcopenshell.util.element.get_pset(pipe, "FI_Geometria", prop="Pituus") / 1000
+
+        if name in qty:
+            if DN in qty[name]:
+                newLength = length + qty[name][DN]
+                qty[name].update({DN : newLength})
+            else:
+                qty[name].update({DN : length})
+        else:
+            qty.update({name : {DN : length}})
+
+    return qty
